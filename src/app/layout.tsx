45d39c775bb9422bec8665496a33ec3ddc19221e
JerRoyDev <jerryroyolsson@gmail.com>
2025-02-20 10:43:20 +0100

    Remove unused import from layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import { initializeSync } from '@/lib/init/db-sync.init';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
