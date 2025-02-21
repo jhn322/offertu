8d2cbf26d66a3e03b83c846296dbdb03379c9b21
jhn322 <johan.soderlund96@gmail.com>
2025-02-21 16:21:15 +0100

    feat: Add getLeads function to retrieve leads
    - Implemented getLeads function in lead.data.ts to fetch leads
    - Created LeadResponse interface in types/index.ts to define lead data structure
    - Ordered leads by creation date in descending order

--- src/server/data/lead.data.ts
+++ src/server/data/lead.data.ts
@@ -1,16 +1,22 @@
 // Import the Prisma client singleton and Lead type
-import { prisma } from '@/lib/db/prisma'
-import { CreateLeadInput } from '@/types'
+import { prisma } from '@/lib/db/prisma';
+import { CreateLeadInput } from '@/types';
 
 // Function to create a new lead in the database
 // Takes an object with email and phone as parameters
 export async function createLeadInDb(data: CreateLeadInput) {
-  return prisma.lead.create({ data })
+  return prisma.lead.create({ data });
+}
+
+export async function getLeads() {
+  return prisma.lead.findMany({
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
 }
 
 //* Example
 // find by id
 // update
-// delete 
-
-
+// delete
